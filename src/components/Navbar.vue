ee684207643f0a020dcbac587a47321caca62686
masculus <magnus.masculus971114@gmail.com>
2022-02-15 13:13:21 -0300

    alterado nome do projeto

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,7 +23,7 @@
            <svg class="w-6 h-6 text-white 3xl" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
           <span
             class="ml-2 text-xl font-bold tracking-wide text-gray-100"
-            >Economizaê</span>
+            >Financiaê</span>
         </NuxtLink>
         
         <ul class="flex items-center hidden ml-auto space-x-8 lg:flex">
