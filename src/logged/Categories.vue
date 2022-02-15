17086cd1ee93a1604c35c31b2aa6a3a4f29100a6
masculus <magnus.masculus971114@gmail.com>
2022-02-15 12:55:07 -0300

    erro de requisição em processo

--- src/logged/Categories.vue
+++ src/logged/Categories.vue
@@ -96,7 +96,7 @@ export default {
 		async getCategories(){
 			
 			try {
-				const response = await axios.get(`http://127.0.0.1:8000/api/categorias/${this.user_id}`, {
+				const response = await axios.get(`http://127.0.0.1:8000/api/categorias/${1}`, {
 					
 					headers: {
 						Authorization: 'Bearer ' + Cookies.get('token_') 
