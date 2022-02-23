21e7bf574da6ca0a5b1616f4a599f6af2351eecf
masculus <magnus.masculus971114@gmail.com>
2022-02-23 19:23:04 -0300

    add delete alert and delete request

--- src/logged/Dashboard.vue
+++ src/logged/Dashboard.vue
@@ -112,6 +112,9 @@
                     <button>
                       <svg class="w-5 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
                     </button>
+                    <a @click="delTransacao(transacao.id)">
+                      <svg class="w-6 h-6 text-white hover:text-red-300 cursor-pointer" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+                    </a>
                   </div>
                 </div>
               </div>
@@ -251,6 +254,51 @@ export default {
 				}
 				console.log(error);
 			}
+		},
+
+    async delTransacao(id){
+      this.$swal.fire({
+        title: 'Voce tem certeza?',
+        text: "Você não poderá reverter isto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, excluir!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          try {
+            const response = axios.delete(`http://127.0.0.1:8000/api/transacoesdelete/${id}`, {
+              
+              headers: {
+                Authorization: 'Bearer ' + Cookies.get('token_') 
+              }
+            }).then(() => {
+              let i = this.transacoes.map(data => data.id).indexOf(id);
+              this.transacoes.splice(i, 1)
+            });
+            // Success 🎉
+            console.log(response)
+          } catch (error) {
+            // Error 😨
+            if (error.response) {
+              console.log(error.response.data);
+              
+            } else if (error.request) {
+              console.log(error.request);
+            } else {
+              console.log('Error', error.message);
+            }
+            console.log(error);
+          }
+          this.$swal.fire(
+            'Excluido!',
+            'Sua transação foi excluída com sucesso.',
+            'success'
+          )
+        }
+      })
+			
 		},
 
     async getTransacoes(){
