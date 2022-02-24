15487c150e08355020b46e6b6b90bfaf4469ef36
masculus <magnus.masculus971114@gmail.com>
2022-02-23 22:58:15 -0300

    add update request with alert

--- src/logged/Dashboard.vue
+++ src/logged/Dashboard.vue
@@ -1,5 +1,31 @@
 <template>
 <Navbar/>
+<div v-show="modal" class="min-w-screen h-screenanimated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id"><div class="absolute bg-black opacity-80 inset-0 z-0"></div>
+    <div class="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
+      <!--content-->
+      <div class="">
+        <!--body-->
+		
+        <div class="text-center p-5 flex-auto justify-center">
+                <svg class="w-6 h-6 w-16 h-16 flex items-center text-red-500 mx-auto" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
+                        <h2 class="text-xl font-bold py-4 ">Editar</h2>
+                        <label>Data</label>
+						<input v-model="editForm.date" id="firstname" type="date" placeholder="Categoria" class="w-full rounded-md focus:ring mb-3 focus:ring-opacity-75 focus:ring-violet-400 dark:border-coolGray-700 dark:text-coolGray-900">
+						<AppFormSelect v-model="editForm.categoria" :list="categories" class="mb-3 "/>
+						<input v-model="editForm.descricao" id="firstname" type="text" placeholder="Categoria" class="w-full rounded-md mb-3 focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-coolGray-700 dark:text-coolGray-900">
+						<input v-model="editForm.valor" id="firstname" type="text" placeholder="Categoria" class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-coolGray-700 dark:text-coolGray-900">
+        </div>
+        <!--footer-->
+        <div class="p-3  mt-2 text-center space-x-4 md:block">
+            <button @click="this.modal = false"  class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+                Cancelar
+            </button>
+            <button @click="updateCategorie" class="mb-2 md:mb-0 bg-green-600 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-green-700">Editar</button>
+        </div>
+		
+      </div>
+    </div>
+</div>
 <AppCardCaixa @abrirLancamento="abrirLancamento" :saldo="$store.state.caixa.saldo"/>
   <div class="max-w-7xl mx-auto p-4">
         <div class="flex items-center justify-between">
@@ -109,9 +135,9 @@
                       </div>
                     </div>
 
-                    <button>
+                    <a @click="editModal(transacao.date, transacao.categoria, transacao.descricao, transacao.valor, transacao.id)">
                       <svg class="w-5 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
-                    </button>
+                    </a>
                     <a @click="delTransacao(transacao.id)">
                       <svg class="w-6 h-6 text-white hover:text-red-300 cursor-pointer" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
                     </a>
@@ -136,8 +162,11 @@ import AppFormSelect from '../components/Ui/AppFormSelect';
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
 import axios from 'axios'
+import { createToast } from 'mosha-vue-toastify';
+import 'mosha-vue-toastify/dist/style.css'
 import Cookies from 'js-cookie'
 import {VMoney} from 'v-money'
+
 
 export default {
   name: 'IndexPage',
@@ -159,7 +188,7 @@ export default {
       loading: true,
       openLancamento: false,
       valorAddCaixa: '',
-  
+      modal: false,
       categories: [],
       user_id: '',
       openTransacao: false,
@@ -169,12 +198,26 @@ export default {
           descricao: '',
           categoria: '',
           user_id: ''
+      },
+      editForm: {
+        date: new Date().toISOString().slice(0, 10),
+        valor: '',
+        descricao: '',
+        categoria: ''
       }
       
     }
   },
 
   methods: {
+    editModal(data, categoria, descricao, valor, id){
+			this.modal = true;			
+			this.editForm.date = data;
+			this.editForm.categoria = categoria;
+			this.editForm.descricao = descricao;
+			this.editForm.valor = valor;
+			this.idModal = id
+		},
     lancarValorCaixa(){
       this.$store.commit('lancarValorCaixa', this.valorAddCaixa)
 
@@ -241,6 +284,7 @@ export default {
 				// Success 🎉
 				console.log(response)
         this.getTransacoes()
+        
         this.limpaCampo()
 			} catch (error) {
 				// Error 😨
@@ -299,6 +343,46 @@ export default {
         }
       })
 			
+		},
+
+    async updateCategorie(){
+			try {
+				const id = this.idModal 
+				/// bota asssimmm ou ASSIM
+				const response = await axios.put(`http://localhost:8000/api/transacoesedit/${id}`, this.editForm, {
+					headers: {
+						Authorization: 'Bearer ' + Cookies.get('token_') 
+					}
+				});
+				// Success 🎉
+				console.log(response)
+				this.getTransacoes();
+				this.modal = false;
+				createToast({
+					title: 'Sucesso!',
+					description: 'A transação foi editada com sucesso!'
+					},
+					{
+					position: 'top-right',
+					transition: 'zoom',
+					hideProgressBar: 'false',
+					toastBackgroundColor: 'green',
+					timeout: 3000,
+					})
+			} catch (error) {
+				// Error 😨
+				if (error.response) {
+					console.log(error.response.data);
+					console.log(this.nameModal)
+					
+				} else if (error.request) {
+					console.log(error.request);
+				} else {
+					console.log('Error', error.message);
+				}
+				//alert(error.message); // pega a desgraca la do erro e alerta
+				console.log(error);
+			}
 		},
 
     async getTransacoes(){
