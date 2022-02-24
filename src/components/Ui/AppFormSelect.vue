5a2e10ac945018586eee80142d1d66af834d93b6
masculus <magnus.masculus971114@gmail.com>
2022-02-23 22:57:33 -0300

    att

--- src/components/Ui/AppFormSelect.vue
+++ src/components/Ui/AppFormSelect.vue
@@ -5,7 +5,7 @@
     </ListboxLabel>
     <div class="mt-1 relative">
       <ListboxButton class="bg-white relative w-full border border-gray-300 rounded-sm shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-        <span class="block truncate">{{ selected!=null? selected.nome : 'Selecione uma opção' }}</span>
+        <span class="block truncate">{{ selected!=null? selected.categoria : 'Selecione uma opção' }}</span>
         <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
           <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
         </span>
